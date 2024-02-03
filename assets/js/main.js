831d80af18385e2fdb1c63dfeb952ee8ca696d2b
freetimezp <kraftukrnet@ukr.net>
2024-02-03 18:45:25 +0200

    06. js-portfolio-45-sundown. Page3 section.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,25 @@ const scroll = new LocomotiveScroll({
 
 
 
+var elemC = document.querySelector("#elem-container");
+var fixed = document.querySelector("#fixed-image");
+elemC.addEventListener("mouseenter", function () {
+    fixed.style.display = "block";
+});
+elemC.addEventListener("mouseleave", function () {
+    fixed.style.display = "none";
+});
+
+var elems = document.querySelectorAll(".elem");
+
+elems.forEach(function (elem) {
+    elem.addEventListener("mouseenter", function () {
+        var image = elem.getAttribute("data-image");
+        fixed.style.backgroundImage = `url(${image})`;
+    });
+});
+
+
 
 
 
